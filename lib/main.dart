99b14080edc7a6dcd110af25e228d4e1f50d1cd7
kei91495 <pleasure91495@gmail.com>
2025-07-02 07:44:47 +0900

    sqflite(SQLite)利用準備

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,58 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final role = prefs.getString('role');
+
   runApp(
     MaterialApp(
-      home: RoleSelectScreen(),
+      home: role == 'parent'
+          ? ParentMainScreen()
+          : role == 'child'
+          ? ChildMainScreen()
+          : RoleSelectScreen(),
       theme: ThemeData(primarySwatch: Colors.teal),
     ),
   );
+}
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+  DBHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('uchimane.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String dbName) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = p.join(directory.path, dbName);
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE points (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user TEXT,
+        amount INTEGER,
+        type TEXT,
+        date TEXT
+      )
+    ''');
+  }
 }
 
 class RoleSelectScreen extends StatelessWidget {
